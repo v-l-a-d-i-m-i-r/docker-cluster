e87a94e8d2ea5b0a880569a4c6d1f865446aadfc
Volodymyr Danilin <volodymyr.daniln@gmail.com>
2020-05-13 22:43:50 +0300

    Update Nextcloud server

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export ALPINE_VERSION='3.11.5';
-export REDIS_VERSION='5.0.8';
-export POSTGRES_VERSION='12.1';
-export NEXTCLOUD_VERSION='17.0.2';
+export ALPINE_VERSION='3.11.6';
+export REDIS_VERSION='6.0.1';
+export POSTGRES_VERSION='12.2';
+export NEXTCLOUD_VERSION='18.0.4';
 export NGINX_VERSION='1.17.9';
 
 
