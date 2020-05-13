ca1a90850e57e7b7a69748dea94b82635842e2db
v-l-a-d-i-m-i-r <volodymyr.danilin@gmail.com>
2020-05-13 11:33:18 +0300

    Update MPD

--- services/mpd/entrypoint.sh
+++ services/mpd/entrypoint.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 
-mkdir /mpd/cache
-mkdir /mpd/playlists
+mkdir -p /mpd/cache
+mkdir -p /mpd/playlists
 
-touch /mpd/cache/tag_cache
-touch /mpd/cache/state
-touch /mpd/cache/sticker.sql
+test -f /mpd/cache/state || touch /mpd/cache/state
+test -f /mpd/cache/tag_cache || touch /mpd/cache/tag_cache
+test -f /mpd/cache/sticker.sql || /mpd/cache/sticker.sql
 # chmod -R 0774 /mpd/cache
 # chown -R mpd: /mpd/cache
-
 
 config_file='/etc/mpd.conf';
 temp_fle='/etc/mpd.conf.tmp';
 
 envsubst < ${config_file} > ${temp_fle};
 mv ${temp_fle} ${config_file};
-
-
-cat /etc/mpd.conf
-
 
 mpd \
   --no-daemon \
